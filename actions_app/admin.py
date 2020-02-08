7023cb20ca4f8ab8ac240e2785f60665667f5e85
Caitlin Powers <cpowers@6river.com>
2020-02-08 11:19:37 -0500

    fix: Register models for admin site

--- actions_app/admin.py
+++ actions_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from actions_app.models import Action, ActionCard, SurveyResponse
 # Register your models here.
+admin.site.register(Action)
+admin.site.register(ActionCard)
+admin.site.register(SurveyResponse)
